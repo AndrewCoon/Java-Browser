db5eb28bfd33b5c98c0f7d787de40492023131e5
Androo <you@example.com>
2022-09-27 09:55:36 -0600

    f

--- Browser.java
+++ Browser.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Browser {
+public class Browser implements ActionListener{
     int size = 450;
     Frame frame;
     Panel panel;
-    JLabel score = new JLabel();
+    protected JTextField textField;
+    protected JTextArea textArea;
+    private final static String newline = "\n";
 
     public Browser() {
         this.size = 450;
@@ -19,6 +24,24 @@ public class Browser {
 
     private void init() {
         frame = new Frame(size, size, "Java Browser");
+
+        textField = new JTextField(20);
+        textField.addActionListener(this);
+
+        textArea = new JTextArea(5, 20);
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridwidth = GridBagConstraints.REMAINDER;
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        frame.add(textField, c);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        frame.add(scrollPane, c);
     }
 
 
