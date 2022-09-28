5e341891451448bd166b2bb14e9caf4759a0245d
Androo <you@example.com>
2022-09-28 09:38:13 -0600

    Added funcitoning enter srotof

--- Browser.java
+++ Browser.java
@@ -2,13 +2,15 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Browser implements ActionListener, KeyListener{
+public class Browser extends JPanel implements ActionListener, KeyListener{
     int size = 450;
     Frame frame;
-    Panel panel;
     protected JTextField textField;
     protected JTextArea textArea;
     private final static String newline = "\n";
+    private static final int CURSOR_WIDTH = 5;  // KG
+    private int ED_WIDTH = 1000;  // KG
+    private int ED_HEIGHT = 100;  // KG
 
     public Browser() {
         this.size = 450;
@@ -21,8 +23,13 @@ public class Browser implements ActionListener, KeyListener{
     }
 
     private void init() {
+        this.ED_WIDTH = size*2;
+        this.ED_HEIGHT = size;
+        
         frame = new Frame(size, size, "Java Browser");
-
+        
+        this.setBounds(0, 0, size, size);
+        frame.add(this);
         textField = new JTextField(20);
         textField.addActionListener(this);
 
@@ -40,6 +47,12 @@ public class Browser implements ActionListener, KeyListener{
         c.weightx = 1.0;
         c.weighty = 1.0;
         frame.add(scrollPane, c);
+
+        frame.getContentPane().add(this,BorderLayout.CENTER);
+        frame.pack();
+        frame.setSize(ED_WIDTH, ED_HEIGHT);
+        frame.addKeyListener(this);
+        frame.setVisible(true);
     }
     public void actionPerformed(ActionEvent evt) {
         
@@ -58,7 +71,7 @@ public class Browser implements ActionListener, KeyListener{
         if (keycode == KeyEvent.VK_ENTER) {
             System.out.println("Enter Pressed");            
         }
-        panel.repaint();
+        this.repaint();
     }
 
 
